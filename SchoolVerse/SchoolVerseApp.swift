efb3f8cdf55a6e5db6bf3bce1571a8e6d7398fa3
Steven Yu <stevenyuser@gmail.com>
2022-08-31 17:53:13 -0400

    Initial Commit

---
+++ SchoolVerse/SchoolVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolVerseApp.swift
+//  SchoolVerse
+//
+//  Created by Steven Yu on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct SchoolVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
