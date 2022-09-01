38773fb618bb2a5747f9ced15342c6c116b91dcb
Steven Yu <stevenyuser@gmail.com>
2022-08-31 22:23:41 -0400

    added firebase life cycle configure()
    firebase configure (chooses .plist) depends on the build scheme (prod or dev)

--- SchoolVerse/SchoolVerseApp.swift
+++ SchoolVerse/SchoolVerseApp.swift
@@ -6,9 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print(CustomEnvironment.firebasePlist)
+        // chooses firebase plist based on prod or dev
+        let filePath = Bundle.main.path(forResource: CustomEnvironment.firebasePlist, ofType: "plist")
+        guard let fileopts = FirebaseOptions(contentsOfFile: filePath!)
+          else { assert(false, "Couldn't load config file") }
+        FirebaseApp.configure(options: fileopts)
+        return true
+    }
+}
 
 @main
 struct SchoolVerseApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
