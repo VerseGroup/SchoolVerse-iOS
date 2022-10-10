5e07fb2faea5770112467f4f8a7b4f69ca22910a
Steven Yu <stevenyuser@gmail.com>
2022-10-09 23:36:25 -0400

    updated schedule view has date picker

--- SchoolVerse/Components/DayScheduleTile.swift
+++ SchoolVerse/Components/DayScheduleTile.swift
@@ -14,17 +14,12 @@ struct DayScheduleTile: View {
         VStack(alignment: .center) {
             Text(day.day.description)
                 .fontWeight(.semibold)
-            Text(
-                day.date
-                    .formatted(date: .complete, time: .omitted)
-                    .dropLast(6)
-            )
-            .fontWeight(.semibold)
         }
-        .font(.headline)
+        .font(.largeTitle)
         .foregroundColor(Color.white)
         .padding(8)
         .frame(maxWidth: .infinity)
         .glassCardFull()
+        .padding()
     }
 }
--- SchoolVerse/Schedule/Views/ScheduleView.swift
+++ SchoolVerse/Schedule/Views/ScheduleView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 // TODO: figure out transitions
+// TODO: auto dismiss date picker when selecting a date
 struct ScheduleView: View {
     @StateObject var vm = ScheduleViewModel()
+    
+    @State var showPicker: Bool = false
+    @AppStorage("accent_color") var accentColor: Color = .accent.cyan
     
     var body: some View {
         ZStack {
@@ -31,9 +35,11 @@ struct ScheduleView: View {
                         
                         Spacer()
                         
-                        if let day = vm.selectedDayEvent {
-                            DayScheduleTile(day: day)
-                        } else {
+                        Button {
+                            withAnimation {
+                                showPicker.toggle()
+                            }
+                        }  label: {
                             Text(vm.selectedDate.weekDateString())
                                 .fontWeight(.semibold)
                                 .font(.headline)
@@ -57,47 +63,69 @@ struct ScheduleView: View {
                         .padding(2)
                     }
                     .padding()
-                    
-                    if let day = vm.selectedDayEvent {
-                        ScrollView(showsIndicators: false) {
-                            // day schedule portion
-                            if let daySchedule = vm.schedule?.days.first(where: { daySchedule in
-                                day.day == daySchedule.day
-                            }) {
-                                // ensures ForEach works since PeriodInfo is not unique, but ForEach requires unique
-                                ForEach(
-                                    // converting PeriodInfo to UniquePeriodInfo
-                                    daySchedule.periods.compactMap({ period in
-                                        UniquePeriodInfo(period: period)
-                                    }).sorted(by: { one, two in
-                                        one.period < two.period
-                                    })
-                                ) { uniquePeriod in
-                                    PeriodTileAndHeader(periodInfo: uniquePeriod.period)
+                    VStack(spacing: 0) {
+                        if let day = vm.selectedDayEvent {
+                            ScrollView(showsIndicators: false) {
+                                DayScheduleTile(day: day)
+                                
+                                // day schedule portion
+                                if let daySchedule = vm.schedule?.days.first(where: { daySchedule in
+                                    day.day == daySchedule.day
+                                }) {
+                                    // ensures ForEach works since PeriodInfo is not unique, but ForEach requires unique
+                                    ForEach(
+                                        // converting PeriodInfo to UniquePeriodInfo
+                                        daySchedule.periods.compactMap({ period in
+                                            UniquePeriodInfo(period: period)
+                                        }).sorted(by: { one, two in
+                                            one.period < two.period
+                                        })
+                                    ) { uniquePeriod in
+                                        PeriodTileAndHeader(periodInfo: uniquePeriod.period)
+                                    }
                                 }
+                                
+                                Spacer()
+                                    .frame(height: 90)
                             }
-                            
-                            Spacer()
-                                .frame(height: 90)
+                            .transition(.opacity)
+                        } else {
+                            VStack {
+                                Spacer()
+                                Text("No Classes Today!")
+                                    .fontWeight(.semibold)
+                                    .font(.largeTitle)
+                                    .foregroundColor(Color.white)
+                                Spacer()
+                                Spacer()
+                            }
+                            .transition(.opacity)
                         }
-                        .transition(.opacity)
-                    } else {
-                        VStack {
-                            Spacer()
-                            Text("No Classes Today!")
-                                .fontWeight(.semibold)
-                                .font(.largeTitle)
-                                .foregroundColor(Color.white)
-                            Spacer()
-                            Spacer()
-                        }
-                        .transition(.opacity)
                     }
+                    .frame(maxWidth: .infinity)
+//                    .background {
+//                        if showPicker {
+//                            Color.black.opacity(0.5)
+//                                .blur(radius: 50)
+//                        }
+//                    }
                     
                 } else {
                     Text("Schedule unavailable")
                 }
             }
+            
+            DatePicker("", selection: $vm.selectedDate, displayedComponents: .date)
+                .datePickerStyle(.graphical)
+                .tint(accentColor)
+                .frame(width: 310, height: 300)
+                .clipped()
+                .background(
+                    .ultraThinMaterial,
+                    in: RoundedRectangle(cornerRadius: 25, style: .continuous)
+                )
+                .opacity(showPicker ? 1 : 0 )
+                .offset(x: 0, y: -100)
         }
         .navigationTitle("Schedule")
     }
