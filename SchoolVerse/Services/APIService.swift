7d187c8dfafc7190c2504d6de61e53f40b9d4174
Steven Yu <stevenyuser@gmail.com>
2023-01-30 10:14:00 -0500

    upgraded to v1.1.3 - approve function now sends iOS version

--- SchoolVerse/Services/APIService.swift
+++ SchoolVerse/Services/APIService.swift
@@ -240,6 +240,7 @@ class APIService: ObservableObject {
         
         let parameters: [String: String] = [
             "user_id": userId,
+            "version": (String(describing: Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String)),
             "api_key": CustomEnvironment.apiKey
         ]
         
