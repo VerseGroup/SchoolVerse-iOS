00869b8b93c0c81993c11d362cd207bed11ad7a3
Steven Yu <stevenyuser@gmail.com>
2023-05-25 23:23:35 -0400

    added clear background view added beautiful graphical date picker to all the calendar screens

--- SchoolVerse/Shared/Components/GraphicalDatePicker.swift
+++ SchoolVerse/Shared/Components/GraphicalDatePicker.swift
@@ -14,10 +14,32 @@ struct GraphicalDatePicker: View {
     
     var body: some View {
         VStack(spacing: 0) {
+            Spacer()
+                .frame(height: 15)
+            
+            HStack {
+                Spacer()
+                
+                Button {
+                    isPresented.toggle()
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color(UIColor.tertiaryLabel))
+                        .scaledToFit()
+                        .frame(
+                            width: 30,
+                            height: 30
+                        )
+                }
+                .padding([.trailing, .vertical])
+            }
+            
             DatePicker("", selection: $selectedDate, displayedComponents: .date)
                 .datePickerStyle(.graphical)
                 .tint(accentColor)
-                
+
             Button(action: {
                 selectedDate = Date.now
             }, label: {
@@ -29,22 +51,26 @@ struct GraphicalDatePicker: View {
                     .background(
                         RoundedRectangle(cornerRadius: 25)
                             .fill(accentColor)
-                        .padding(5)
+                            .padding(5)
                     )
             })
             
+            Spacer()
         }
-        .frame(width: 320, height: 380)
-        .clipped()
-        .background(
-            .ultraThinMaterial,
-            in: RoundedRectangle(cornerRadius: 25, style: .continuous)
-        )
-        .opacity(isPresented ? 1 : 0)
+        .if(UIDevice.current.userInterfaceIdiom == .pad) { view in
+            view
+                .frame(width: 400, height: 500)
+        }
         .onChange(of: selectedDate) { _ in
             withAnimation {
                 isPresented = false
             }
         }
+        .clearModalBackground() // in ios 16.4 depreciate and use .presentationBackground(.clear)
+        .background(
+            .thinMaterial,
+            in: RoundedRectangle(cornerRadius: 25, style: .continuous)
+        )
+        .ignoresSafeArea(edges: .bottom)
     }
 }
